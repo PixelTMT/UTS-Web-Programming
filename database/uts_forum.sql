82ddd7659ca6f69f2c766cd42a6a7668c6233966
Grego <ivanhalim888@gmail.com>
2022-10-07 13:22:00 +0700

    added database and saving login and post data

--- database/uts_forum.sql
+++ database/uts_forum.sql
@@ -1,59 +1,42 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 03, 2022 at 02:13 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+use uts_forum;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `uts_forum`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` varchar(5) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `user_key` varchar(30) NOT NULL,
-  `encrypted_password` varchar(50) NOT NULL,
-  `img` varchar(50) NOT NULL
+CREATE TABLE user (
+  id VARCHAR(5) PRIMARY KEY,
+  username VARCHAR(30) NOT NULL,
+  email VARCHAR(45) NOT NULL,
+  user_key VARCHAR(30) NOT NULL,
+  encrypted_password VARCHAR(50) NOT NULL,
+  img VARCHAR(50),
+  roles smallint(1)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `user`
---
+CREATE TABLE forum (
+  id VARCHAR(5) PRIMARY KEY,
+  categories VARCHAR(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `user` (`id`, `username`, `email`, `user_key`, `encrypted_password`, `img`) VALUES
-('00001', 'GzHKl_Uir21VTs]!', 'Jen5.@mgai.copm', 'q#$W3oqdc#fZa#E', '$gDkkeo4G9w0Hb', 'penculik A.png');
+CREATE TABLE post (
+  id VARCHAR(5) NOT NULL PRIMARY KEY,
+  title VARCHAR(300) NOT NULL,
+  body VARCHAR(99999) NOT NULL,
+  like_ammount INT NOT NULL,
+  comment_ammount INT NOT NULL,
+  time_created TIME NOT NULL,
+  date_created DATE NOT NULL,
+  user_id VARCHAR(5) NOT NULL,
+  forum_id VARCHAR(5) NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(forum_id) REFERENCES forum(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE comment (
+  user_id VARCHAR(5),
+  post_id VARCHAR(5),
+  body    VARCHAR(5000) NOT NULL,
+  time_created TIME NOT NULL,
+  date_created DATE NOT NULL,
+  PRIMARY KEY(user_id, post_id),
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(post_id) REFERENCES post(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
